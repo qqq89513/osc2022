efe84368ac81b44f3ac101dfa2e8e734cd0c91e8
Quency Lin <qqq89513@yahoo.com.tw>
2022-05-25 01:02:23 +0800

    lab6: Feat: Finish basic1 last part: Finer Granularity Paging

--- api/src/mmu.c
+++ api/src/mmu.c
@@ -10,12 +10,11 @@ extern uint64_t pg_dir;
 #define MAIR_NORMAL_NOCACHE     0b01000100  // high 0100:Normal memory, Outer Non-cacheable; low 0100:Normal memory, Inner Non-cacheable
 #define MAIR_IDX_DEVICE_nGnRnE  0           // set for Attr0
 #define MAIR_IDX_NORMAL_NOCACHE 1           // set for Attr1
+#define MAIR_SHIFT              2
 
 #define PD_TABLE 0b11
 #define PD_BLOCK 0b01
 #define PD_ACCESS (1 << 10)
-#define BOOT_PGD_ATTR PD_TABLE
-#define BOOT_PUD_ATTR (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK) // device mem, points to block
 
 #define KERNEL_VM_TO_PM_MASK 0X0000FFFFFFFFFFFF // for kernel, virtual mem addr to physical mem addr
 
@@ -23,16 +22,34 @@ void mmu_init(){
   write_sysreg(tcr_el1, TCR_CONFIG_DEFAULT);
 
   write_sysreg(mair_el1, 
-    (MAIR_DEVICE_nGnRnE  << (MAIR_IDX_DEVICE_nGnRnE * 8)) | 
-    (MAIR_NORMAL_NOCACHE << (MAIR_IDX_NORMAL_NOCACHE * 8)) );
+    (MAIR_DEVICE_nGnRnE  << (MAIR_IDX_DEVICE_nGnRnE * 8)) |   // set MAIR attr0
+    (MAIR_NORMAL_NOCACHE << (MAIR_IDX_NORMAL_NOCACHE * 8)) ); // set MAIR attr1
 
   uint64_t *PGD = (uint64_t*)( (uint64_t)(&pg_dir) & KERNEL_VM_TO_PM_MASK ); // pg_dir is from link.ld, remove 0xFFFF000000000000 to access phy address
-  uint64_t *PUD = (uint64_t*)((uint64_t)PGD + 0x1000);
-  uint64_t *PMD = (uint64_t*)((uint64_t)PGD + 0x2000);
+  uint64_t *PUD = (uint64_t*)((uint64_t)PGD + 0x1000);  // L1 table, entry points to L2 table or 1GB block
+  uint64_t *PMD = (uint64_t*)((uint64_t)PGD + 0x2000);  // L2 table, entry points to L3 table or 2MB block
 
-  PGD[0] = (uint64_t)PUD | BOOT_PGD_ATTR;
-  PUD[0] = 0x00000000 | BOOT_PUD_ATTR;  // 1st 1GB mapped by the 1st entry of PUD
-  PUD[1] = 0x40000000 | BOOT_PUD_ATTR;  // 2nd 1GB mapped by the 2nd entry of PUD
+  // Set Identity Paging
+  // 0x00000000 ~ 0x3f000000: Normal  // PUD[0]
+  // 0x3f000000 ~ 0x40000000: Device  // PUD[0]
+  // 0x40000000 ~ 0x80000000: Device  // PUD[1]
+  // PGD[0] = (uint64_t)PUD | (1LL<<63) | (1LL<<60) | PD_TABLE;  // 1st entry points to a L1 table ??
+  PGD[0] = (uint64_t)PUD | PD_TABLE;  // 1st entry points to a L1 table
+  PUD[0] = (uint64_t)PMD | PD_TABLE;  // 1st 1GB mapped by L3 table, where L3 table pointed by 1st entry of PUD
+  PUD[1] = 0x40000000 | (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << MAIR_SHIFT) | PD_BLOCK);     // 2nd 1GB mapped by the 2nd entry of PUD
+
+  // Note for following for:
+  //    <<21 because each entry of PMD is 2MB=1<<21
+  //    504 = 0x3f000000 / 2MB
+  //    512 = 0x40000000 / 2MB
+
+  // 0x00000000 ~ 0x3f000000: Normal
+  for(uint64_t i=0; i<504; i++)
+    PMD[i] = (i << 21) | PD_ACCESS | (MAIR_IDX_NORMAL_NOCACHE<<MAIR_SHIFT) | PD_BLOCK;
+
+  // 0x40000000 ~ 0x80000000: Device
+  for(uint64_t i=504; i<512; i++)
+    PMD[i] = (i << 21) | PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE<<MAIR_SHIFT) | PD_BLOCK;
 
   write_sysreg(ttbr0_el1, PGD);         // load PGD to the bottom translation-based register.
   write_sysreg(ttbr1_el1, PGD);         // also load PGD to the upper translation based register.
